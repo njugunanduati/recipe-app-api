806cb6218ae82b9de03d5b5ff5eab4e50ee39209
James Njuguna Nduati <njugunanduati@gmail.com>
2019-01-07 11:41:39 +0300

    added the postgres db configuration and the wait_for_db command

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -15,7 +14,7 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-    
+
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'james@GMAIL.com'
